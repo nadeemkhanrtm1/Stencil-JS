03e547ad8a1b86fddf87250f29355ac248c546cf
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2021-02-01 16:06:20 +0530

    1st commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,18 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  render() {
+    return <div class='App'> Hello I am {this.first} </div>;
+  }
+}
